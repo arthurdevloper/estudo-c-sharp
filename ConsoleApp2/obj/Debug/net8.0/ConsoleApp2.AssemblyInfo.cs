03e408923919e7e85eb3596bd5e9d722cfcebf55
arthurdevloper <arthuralann.dev@gmail.com>
2024-11-19 22:49:54 -0300

    adicionado gitignore

--- ConsoleApp2/obj/Debug/net8.0/ConsoleApp2.AssemblyInfo.cs
+++ ConsoleApp2/obj/Debug/net8.0/ConsoleApp2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a5ea2d0e3d2f1a8ef99b12ef39de975896b9e4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
