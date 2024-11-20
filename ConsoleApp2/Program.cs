7a5ea2d0e3d2f1a8ef99b12ef39de975896b9e4a
arthurdevloper <arthuralann.dev@gmail.com>
2024-11-19 22:38:02 -0300

    criacao do arquivo base do estudo c-sharp

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CalculadoraDeImpostoDeRenda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite seu salário:");
+            double salario = double.Parse(Console.ReadLine());  
+
+            double imposto = 0;
+
+            if (salario <= 1903.98)
+            {
+                imposto = 0;  
+            }
+            else if (salario <= 2826.65)
+            {
+                imposto = salario * 0.075;  
+            }
+            else if (salario <= 3751.05)
+            {
+                imposto = salario * 0.15;  
+            }
+            else if (salario <= 4664.68)
+            {
+                imposto = salario * 0.225;  
+            }
+            else
+            {
+                imposto = salario * 0.275;  
+            }
+
+            Console.WriteLine("Imposto a pagar: R$" + imposto);
+            Console.WriteLine("Salário líquido: R$" + (salario - imposto));
+        }
+    }
+}
+
